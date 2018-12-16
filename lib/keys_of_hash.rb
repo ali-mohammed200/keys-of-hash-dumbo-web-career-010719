c9696ed73bf78acc4eab26a19eddaaaa6b0f0ac1
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-16 03:04:01 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,10 @@
 class Hash
   def keys_of(*arguments)
   #  map {|key, value| args.include?(value) ? key : nil }.compact
-    map {|key, value| key}
+    map {|key, value|
+      if arguments.include?(value)
+        key
+      end
+    }
   end
 end
